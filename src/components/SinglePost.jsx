b75a27f7941f1a83b4e5a86d89c0551ee5fc5164
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-10 11:40:34 +0300

    read more 2p

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 export default function SinglePost(props) {
+  const [isTextFull, setIsTextFull] = useState(false);
+  function toggleFullText() {
+    setIsTextFull(!isTextFull);
+  }
+
   const { title, body, reactions } = props.item;
   return (
     <li className='postCard'>
       <div>
         <h3>{title}</h3>
-        <p>{body.slice(0, 45)}...</p>
+        {!isTextFull && <p>{body.slice(0, 45)}...</p>}
+        {isTextFull && <p>{body}</p>}
+        <button onClick={toggleFullText}>read more</button>
         <p>{reactions} people like this</p>
       </div>
     </li>
