7b71483cd1b6617f72f3c22a92e3ef06f20c338f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-11 09:03:17 +0300

    sortBySelectedValue

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,10 +4,22 @@
 import { useEffect, useState } from 'react';
 import SinglePost from './SinglePost';
 
+const sortOptions = ['id', 'title', 'reactions'];
+
 const postsUrl = 'https://dummyjson.com/posts';
 
 export default function PostsList() {
   const [postsArr, setPostsArr] = useState([]);
+  const [selectValue, setSelectValue] = useState('title');
+
+  function selectInput(event) {
+    console.log('select happened', event.target.value);
+    setSelectValue(event.target.value);
+  }
+
+  function sortBySelectedValue() {
+    // sort by selectValue
+  }
 
   useEffect(() => {
     fetch(postsUrl)
@@ -15,17 +27,43 @@ export default function PostsList() {
       .then((dataBack) => {
         console.log('dataBack.posts ===', dataBack.posts);
         // irasyti posts is dataBack i postsArr
-        setPostsArr(dataBack.posts);
+        setPostsArr(dataBack.posts.slice(0, 5));
       })
       .catch((error) => {
         console.warn('ivyko klaida:', error);
       });
   }, []);
 
+  const howManyPosts = postsArr.length;
+
+  function sortByReactions() {
+    console.log('sortByReactions ===');
+    // pasidartyti state kopija
+    const postsArrCopy = [...postsArr];
+    // isrikiuoti kopija
+    postsArrCopy.sort((aObj, bObj) => bObj.reactions - aObj.reactions);
+    // atnaujinti state su kopija
+    console.table(postsArrCopy);
+    setPostsArr(postsArrCopy);
+  }
+
   return (
     <div>
       <h2>PostsList</h2>
-      <p>Kiek viso</p>
+      <p>Kiek viso: {howManyPosts}</p>
+      <button onClick={sortByReactions}>sort by reactions</button>
+      <p>sort by: {selectValue}</p>
+      <select onChange={selectInput} value={selectValue}>
+        <option disabled value=''>
+          --select sort option--
+        </option>
+        {sortOptions.map((optValue) => (
+          <option key={optValue} value={optValue}>
+            {optValue}
+          </option>
+        ))}
+      </select>
+      <button onClick={sortBySelectedValue}>Sort</button>
       <ul className='unlisted grid'>
         {postsArr.map((pObj) => (
           <SinglePost key={pObj.id} item={pObj} />
